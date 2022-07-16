7aca575a0f4d4faed95e27d77d050ad0650ea6de
Arnav Thorat <arnavthorat78@gmail.com>
2022-07-16 12:07:38 +1000

    Added the road

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -11,6 +13,12 @@ function animate() {
 
   canvas.height = window.innerHeight;
 
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
