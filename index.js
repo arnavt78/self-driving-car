45a63ea22907dcd61dbd400043dacf4e902a2404
arnavthorat78 <arnavthorat78@gmail.com>
2024-04-01 19:01:20 +1100

    Added controls for player-driven car

---
+++ index.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("myCanvas");
+
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
+const animate = () => {
+  car.update();
+  canvas.height = window.innerHeight;
+  car.draw(ctx);
+
+  requestAnimationFrame(animate);
+};
+
+animate();
