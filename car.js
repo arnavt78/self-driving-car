7de2d0e57311bcb3947ff7e93173c44ae8f5fe85
arnavthorat78 <arnavthorat78@gmail.com>
2024-04-02 07:00:50 +1100

    Non-functional code; error with `sensor.js`

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -76,5 +78,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
---
+++ utils.js
@@ -0,0 +1,1 @@
+const lerp = (a, b, t) => a + (b - a) * t;
