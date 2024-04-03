774015fabb72c64134e6b0b50594e0a7b89d3830
arnavthorat78 <arnavthorat78@gmail.com>
2024-04-03 16:51:53 +1100

    Added polygons and beginning of damage

--- car.js
+++ car.js
@@ -10,6 +10,7 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+    this.damaged = false;
 
     this.sensor = new Sensor(this);
     this.controls = new Controls();
@@ -17,7 +18,46 @@ class Car {
 
   update(roadBorders) {
     this.#move();
+
+    this.polygon = this.#createPolygon();
+    this.damaged = this.#assessDamage(roadBorders);
+
     this.sensor.update(roadBorders);
+  }
+
+  #assessDamage(roadBorders) {
+    for (let i = 0; i < roadBorders.length; i++) {
+      if (polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+
+    return points;
   }
 
   #move() {
@@ -69,15 +109,14 @@ class Car {
   }
 
   draw(ctx) {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
-
     ctx.beginPath();
-    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
-    ctx.fill();
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
 
-    ctx.restore();
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
+
+    ctx.fill();
 
     this.sensor.draw(ctx);
   }
--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
-animate();
-
-function animate() {
+const animate = () => {
   car.update(road.borders);
 
   canvas.height = window.innerHeight;
@@ -20,4 +18,6 @@ function animate() {
 
   ctx.restore();
   requestAnimationFrame(animate);
-}
+};
+
+animate();
